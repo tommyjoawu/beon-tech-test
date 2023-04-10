75565b185c6719d0cde7156a75b91429f86afe8c
Rhet Prieto <rhetprieto@gmail.com>
2023-04-10 14:42:38 -0600

    Add happy path test for tweet requests

--- spec/requests/api/tweet_request_spec.rb
+++ spec/requests/api/tweet_request_spec.rb
@@ -1,7 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "API Tweets", type: :request do
+  describe "#create" do
+    let(:response_body) { JSON.parse(response.body) }
 
-  # See user_request_spec for inspiration
-  
+    context 'with valid parameters' do
+      let(:user1) { create(:user)}
+      let(:valid_body) { 'This is a valid tweet' }
+
+      it 'returns a successful response' do
+        post api_tweets_path(user_id: user1.id, body: valid_body)
+
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'creates a new tweet' do
+        expect {
+          post api_tweets_path(user_id: user1.id, body: valid_body)
+        }.to change(Tweet, :count).by(1)
+      end
+    end
+
+    xcontext 'With invalid parameters' do
+      let(:user1) { create(:user)}
+
+      context 'When the body is too long' do
+        let(:invalid_body) {  }
+
+        it 'returns an error response' do
+
+        end
+
+        it 'does not create a new tweet' do
+
+        end
+      end
+
+      context 'When the tweet might be a duplicate' do
+
+      end
+    end
+  end
 end
